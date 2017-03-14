06f292989c6fa59f37a6000b20ef1ecab09b8b0f
maggie jv <maggie jv>
2017-03-14 08:44:55 -0600

    stack-1

---
+++ stack.js
@@ -0,0 +1,32 @@
+function Stack(){
+  var nombres =[];
+  this.push= function(elemento){
+    nombres.push(elemento);
+  };
+  this.pop =function(){
+    return nombres.pop();
+  };
+  this.mostrar = function(){
+    document.write(nombres.toString());
+  };
+};
+var nombre = new Stack()
+nombre.push("Mireya");
+nombre.push("Miguel");
+nombre.push("Magaly");
+nombre.push("Luna");
+nombre.push("Danae");
+
+var apellidos = new Stack()
+apellidos.push("Martinez");
+apellidos.push("Rodriguez");
+apellidos.push("Cervantes");
+apellidos.push("Granados");
+apellidos.push("Reynoso");
+var nombres= [];
+nombres.push(nombre.pop()+" "+apellidos.pop());
+nombres.push(nombre.pop()+" "+apellidos.pop());
+nombres.push(nombre.pop()+" "+apellidos.pop());
+nombres.push(nombre.pop()+" "+apellidos.pop());
+nombres.push(nombre.pop()+" "+apellidos.pop());
+document.write(nombres);
